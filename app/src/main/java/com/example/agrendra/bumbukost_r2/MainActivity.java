0001c1c36aaa8b00553778f6fecfe864e7add803
Ilyasdavid <ilyasds520@gmail.com>
2017-11-25 13:15:51 +0700

    fix

--- app/src/main/java/com/example/agrendra/bumbukost_r2/MainActivity.java
+++ app/src/main/java/com/example/agrendra/bumbukost_r2/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
 import android.view.MenuItem;
 
