882c3135359f2d191310b3f8547d12b09ab2851b
agrendra <agerf11@gmail.com>
2018-01-03 10:30:17 +0700

    List boy #MENUJU_WISUDA_2018

--- app/src/main/java/com/example/agrendra/bumbukost_r2/ListActivity.java
+++ app/src/main/java/com/example/agrendra/bumbukost_r2/ListActivity.java
@@ -1,33 +1,20 @@
 package com.example.agrendra.bumbukost_r2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListActivity extends AppCompatActivity {
 
-    Intent intent = new Intent();
-    ListView listView;
-
-    String[] itemname = {
-            "Cabe Hijau",
-            "Cabe Merah",
-            "Jahe",
-            "Kunyit",
-            "Lengkuas"
-    };
-
-    Integer[] imgid = {
-            R.drawable.cabe_hijau,
-            R.drawable.cabe_merah,
-            R.drawable.jahe,
-            R.drawable.kunyit,
-            R.drawable.lengkuas
-    };
+    GridLayout gridLayout;
 
 
 
@@ -36,52 +23,51 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        gridLayout = (GridLayout) findViewById(R.id.mainGrid);
 
-        CustomListAdapter adapter = new CustomListAdapter(this, itemname, imgid);
-        listView = (ListView)findViewById(R.id.data);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                switch (position){
-                    case 0:
-                        intent = new Intent(ListActivity.this, DetailActivity.class);
-                        intent.putExtra("judul", "Cabe Hijau");
-                        intent.putExtra("detail", "Cabe cabe an lu boy");
-                        intent.putExtra("gambar", R.drawable.cabe_hijau);
-                        startActivity(intent);
-                        break;
-                    case 1:
-                        intent.putExtra("judul", "Cabe Hijau");
-                        intent.putExtra("detail", "Cabe cabe an lu boy merah");
-                        intent.putExtra("gambar", R.drawable.cabe_merah);
-                        startActivity(intent);
-                        break;
-                    case 2:
-                        intent.putExtra("judul", "Jahe");
-                        intent.putExtra("detail", "Jahe merupakan rimpang berbentuk ruas dengan warna kulit coklat muda yang sering digunakan untuk bahan utama minuman hangat dan bumbu tambahan. Selain itu, jahe juga digunakan dalam industri obat-obatan. Jahe memiliki rasa yang pedas dan bau yang menyengat namun menyegarkan. Bau dan rasa pedas pada jahe disebabkan oleh senyawa utamanya, keton yaitu Zingeron.");
-                        intent.putExtra("gambar", R.drawable.jahe);
-                        startActivity(intent);
-                        break;
-                    case 3:
-                        intent.putExtra("judul", "Kunyit");
-                        intent.putExtra("detail", "Laos ditambahkan dalam masakan agar memiliki aroma yang segar, umumnya laos pasti digunakan dalam masakan bersantan, atau masakan yang melibatkan daging seperti ayam, ikan atau sapi.Laos tidak memiliki rasa yang pedas seperti jahe. Ruas pada laos juga berbeda dengan jahe yang dimana laos memiliki ukuran yang relatif besar dengan kulit berwarna coklat tua. Selain itu, serat pada laos lebih banyak bila dibandingkan dengan jahe sehingga menjadikan laos ketika dipotong dengan pisau terasa lebih sulit daripada jahe.");
-                        intent.putExtra("gambar", R.drawable.kunyit);
-                        startActivity(intent);
-                        break;
-                    case 4:
-                        intent.putExtra("judul", "Lengkuas");
-                        intent.putExtra("detail", "Kunyit merupakan bahan utama dalam pembuatan bumbu kuning karena daging kunyit berwarna kuning tua hampir ke jingga. Kunyit digunakan sebagai pewarna alami karena dapat memberikan warna alami kuning pada masakan. Selain itu, kunyit juga memberikan rasa dan aroma segar pada masakan. Satu ruas kelingking kunyit sudah cukup untuk membuat 1 ayam opor kuning.");
-                        intent.putExtra("gambar", R.drawable.kunyit);
-                        startActivity(intent);
-                        break;
-                    default:
-                        break;
+        setSingleEvent(gridLayout);
+
+    }
+
+    private void setToggleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+                        Toast.makeText(ListActivity.this, "State : True", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Toast.makeText(ListActivity.this, "State : False", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
+            });
+        }
+    }
 
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(ListActivity.this,MenuActivity.class);
+                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                    startActivity(intent);
+
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/agrendra/bumbukost_r2/MenuActivity.java
+++ app/src/main/java/com/example/agrendra/bumbukost_r2/MenuActivity.java
@@ -3,59 +3,33 @@ package com.example.agrendra.bumbukost_r2;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MenuActivity extends AppCompatActivity {
-Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-
         setContentView(R.layout.activity_menu);
+
+        TextView txtInfo = (TextView)findViewById(R.id.txtInfo);
+        if(getIntent() != null)
+        {
+            String info = getIntent().getStringExtra("info");
+            txtInfo.setText(info);
+        }
     }
 
-    public void rendang(View view) {
-
-        intent = new Intent(this, MainActivity.class);
-        intent.putExtra("title","Rendang");
-        intent.putExtra("picture", R.drawable.rendang);
-        intent.putExtra("detail","Merupakan makanan Yang berasal dari Padang Sumatra Barat, " +
-                "dan menjadi makanan paling diminati dan terlezat di tahun 2016 oleh Genius World Record, " +
-                "dan menjadi kebanggaan tersendiri Bagi Indonesia serta bagi Rakyat Minangkabau sendiri");
-        startActivity(intent);
-
-        /*Intent intent = new Intent(MenuActivity.this, MainActivity.class);
-        MenuActivity.this.startActivity(intent);
-        MenuActivity.this.finish();*/
-    }
-
-
-    public void bumbu(View view) {
-        //oke
-    }
-
-    public void minum(View view) {
-    }
-
-    public void sarapan(View view) {
-    }
-
-    public void cemilan(View view) {
-    }
-
-    public void rempah(View view) {
-        Intent intent = new Intent(this, ListActivity.class);
-        startActivity(intent);
-    }
 }
 
